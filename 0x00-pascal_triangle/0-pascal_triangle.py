7cfce0d24290e9d36177fd364f5149f008c578a4
tegbiye <se.tegbabu.solomon@gmail.com>
2024-08-08 23:21:33 +0000

    Added the task 0 and test file and gitignore

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,25 @@
+def pascal_triangle(n):
+  """Generates Pascal's triangle up to the nth row.
+
+  Args:
+    n: The number of rows in the Pascal's triangle.
+
+  Returns:
+    A list of lists representing Pascal's triangle, where each inner list
+    represents a row.
+  """
+
+  if n <= 0:
+    return []
+
+  triangle = [[1]]
+  for i in range(1, n):
+    prev_row = triangle[i - 1]
+    row = [1]
+    for j in range(1, i):
+      row.append(prev_row[j - 1] + prev_row[j])
+    row.append(1)
+    triangle.append(row)
+
+  return triangle
+

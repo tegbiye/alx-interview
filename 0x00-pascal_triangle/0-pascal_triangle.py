29545dd0c389d81af0b9f53e9b86398ed71ec2e8
tegbiye <se.tegbabu.solomon@gmail.com>
2024-08-08 23:52:28 +0000

    fixed the format

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,28 +3,28 @@
 0-pascal_triangle
 """
 
+
 def pascal_triangle(n):
-  """Generates Pascal's triangle up to the nth row.
+    """Generates Pascal's triangle up to the nth row.
 
-  Args:
-    n: The number of rows in the Pascal's triangle.
+    Args:
+      n: The number of rows in the Pascal's triangle.
 
-  Returns:
-    A list of lists representing Pascal's triangle, where each inner list
-    represents a row.
-  """
+    Returns:
+      A list of lists representing Pascal's triangle, where each inner list
+      represents a row.
+    """
 
-  if n <= 0:
-    return []
+    if n <= 0:
+        return []
 
-  triangle = [[1]]
-  for i in range(1, n):
-    prev_row = triangle[i - 1]
-    row = [1]
-    for j in range(1, i):
-      row.append(prev_row[j - 1] + prev_row[j])
-    row.append(1)
-    triangle.append(row)
+    triangle = [[1]]
+    for i in range(1, n):
+        prev_row = triangle[i - 1]
+        row = [1]
+        for j in range(1, i):
+            row.append(prev_row[j - 1] + prev_row[j])
+        row.append(1)
+        triangle.append(row)
 
-  return triangle
-
+    return triangle
